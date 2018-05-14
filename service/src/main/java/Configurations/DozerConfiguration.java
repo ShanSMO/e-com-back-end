e0dd4b28e2f7d6303ae16eb2f532c019655416d8
Shanaka <madushanka991@gmail.com>
2018-05-14 22:30:59 +0530

    updated

--- service/src/main/java/Configurations/DozerConfiguration.java
+++ service/src/main/java/Configurations/DozerConfiguration.java
@@ -1,19 +1,11 @@
 package Configurations;
 
-import Dtos.BusinessOwnerDto;
-import Dtos.CompanyDto;
-import Dtos.DeviceDto;
-import Dtos.UserDto;
-import Entities.BusinessOwner;
-import Entities.Company;
-import Entities.Device;
-import Entities.User;
 import org.dozer.DozerBeanMapper;
 import org.dozer.loader.api.BeanMappingBuilder;
 import org.dozer.loader.api.TypeMappingOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import static org.dozer.loader.api.FieldsMappingOptions.useMapId;
+
 import static org.dozer.loader.api.TypeMappingOptions.mapId;
 import static org.dozer.loader.api.TypeMappingOptions.mapNull;
 
@@ -40,26 +32,7 @@ public class DozerConfiguration{
         return new BeanMappingBuilder() {
             @Override
             protected void configure() {
-                mapping(User.class,UserDto.class, TypeMappingOptions.oneWay(),mapId("USER_MAP"),mapNull(true))
-                    .exclude("password").exclude("owner");
 
-                mapping(Company.class, CompanyDto.class, TypeMappingOptions.oneWay(),mapId("COMPANY_MAP"),mapNull(true))
-                    .exclude("user");
-
-                mapping(Company.class, CompanyDto.class, TypeMappingOptions.oneWay(),mapId("COMPANY_MAP_NO_PASSWORD"),mapNull(true))
-                    .exclude("user.password");
-
-                mapping(Device.class,DeviceDto.class , TypeMappingOptions.oneWay(),mapId("DEVICE_EXCLUDE_COMPANY"),mapNull(true))
-                    .exclude("company");
-
-                mapping(BusinessOwner.class, BusinessOwnerDto.class , TypeMappingOptions.oneWay() , mapId("BO_MAP_NO_COMPANY") ,
-                    mapNull(true))
-                    .exclude("user")
-                    .exclude("company");
-
-                mapping(Device.class, DeviceDto.class , TypeMappingOptions.oneWay() , mapId("MAP_DEVICE_NO_LOCATION") ,
-                    mapNull(true))
-                    .exclude("locations");
 
             }
         };
